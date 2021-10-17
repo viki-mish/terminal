cc2039638f538d48ce79f504417b2beee49ac3e9
viki-mish <62746128+viki-mish@users.noreply.github.com>
2021-10-17 22:43:36 +0530

    Add files via upload

--- assignment.cpp
+++ assignment.cpp
@@ -534,6 +534,69 @@ void create_file(vector<string> v){
 
 }
 
+
+void delete_file(string path)
+{
+    char Path[100];
+    strcpy(Path, path.c_str());
+    
+    int fd=remove(Path);
+    
+    if(fd==-1){
+        cout<<"error in deleting file "<<endl;
+        exit(0);
+    }
+}
+
+
+int search(const char *path,string file) {
+   DIR *d = opendir(path);
+   size_t path_len = strlen(path);
+   int r = -1;
+   int f = 1;
+   if (d) {
+      struct dirent *p;
+
+      r = 0;
+      while (!r && (p=readdir(d))) {
+          int r2 = -1;
+          char *buf;
+          size_t len;
+
+          /* Skip the names "." and ".." as we don't want to recurse on them. */
+          if (!strcmp(p->d_name, ".") || !strcmp(p->d_name, ".."))
+             continue;
+
+          len = path_len + strlen(p->d_name) + 2; 
+          buf = (char*)malloc(len);
+
+          if (buf) {
+             struct stat statbuf;
+
+             snprintf(buf, len, "%s/%s", path, p->d_name);
+             if (!stat(buf, &statbuf)) {
+                if (S_ISDIR(statbuf.st_mode))
+                   r2 = search(buf, file);
+                else if(strcmp(p->d_name,file.c_str()))
+                   {cout<<"TRUE";
+               		break;
+               		f = 0;}
+             }
+          }
+          r = r2;
+      }
+      closedir(d);
+      if(f)cout<<"FALSE";
+   }
+
+   if (!r)
+      r = rmdir(path);
+
+   return r;
+}
+
+
+
 int delete_dir(const char *path) {
    DIR *d = opendir(path);
    size_t path_len = strlen(path);
@@ -553,7 +616,7 @@ int delete_dir(const char *path) {
              continue;
 
           len = path_len + strlen(p->d_name) + 2; 
-          buf = malloc(len);
+          buf = (char*)malloc(len);
 
           if (buf) {
              struct stat statbuf;
@@ -685,11 +748,13 @@ void cmd(){
 				case 5:
 					break;
 				case 6:
+					delete_file(commands[1]);
 					break;
 				case 7:
-					delete_dir(commands[1].c_str())
+					delete_dir(commands[1].c_str());
 					break;
 				case 8:
+					search(".",commands[1]);
 					break;
 				case 9:
 					break;
